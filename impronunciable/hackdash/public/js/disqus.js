3709b27b8a15f854e166d2cfb6214552af1b0f92
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-23 03:56:52 +0100

    annotation

--- impronunciable/hackdash/public/js/disqus.js
+++ impronunciable/hackdash/public/js/disqus.js
@@ -1,7 +1,9 @@
+/* @flow */
+
 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 var scripts = document.getElementsByTagName( 'script' );
 var thisScriptTag = scripts[ scripts.length - 1 ];
-var disqus_shortname = thisScriptTag.getAttribute('disqus_shortname') ; // required: replace example with your forum shortname
+var disqus_shortname: string | null = thisScriptTag.getAttribute('disqus_shortname') ; // required: replace example with your forum shortname
 if ( disqus_shortname != '' ) {
   var disqus_config = function () { 
     this.language = "en_EN";
