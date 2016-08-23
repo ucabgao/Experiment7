5bc70dc248434e7d1aed6f3a621ebd1aed529c46
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-23 04:59:09 +0100

    annotation

--- naholyr/connect-access-control/lib/access-control.ts
+++ naholyr/connect-access-control/lib/access-control.ts
@@ -1,89 +1,102 @@
+
 /*!
  * Connect - access-control
  * Copyright(c) 2011 Nicolas Chambrier
  * MIT Licensed
  */
+
 /**
  * Dependencies
  */
-var User = require('./user'), utils = require('./utils');
+const User = require('./user')
+    , utils = require('./utils')
+
 /**
  * Expose the middleware
  */
 exports = module.exports = accessControl;
+
 /**
  * Paths to ignored, default "/favicon.ico"
  */
 exports.ignoredPaths = ["/favicon.ico"];
+
 /**
  * Global protection
  */
 exports.securedPaths = [];
 exports.requiredLoggedIn = false;
 exports.requiredRoles = [];
+
 /**
  * Middleware called when user does not match required roles
  */
 exports.limitedAccessCallback = function (req, res) {
-    res.writeHead(403, { "Content-Type": "text/plain" });
-    res.end("Forbidden");
-};
+  res.writeHead(403, {"Content-Type": "text/plain"});
+  res.end("Forbidden");
+}
+
 /**
  * Middleware called when user is not logged in
  */
 exports.loginPath = "/login"; // Path to login form
 exports.notLoggedInCallback = function (req, res) {
-    var url = exports.loginPath;
-    if (!~url.indexOf('://')) {
-        url = 'http' + (req.connection.encrypted ? 's' : '') + '://' + req.headers.host + url;
-    }
-    res.writeHead(302, { "Location": url, "Content-Type": "text/html" });
-    res.end('<p>Redirecting to <a href="' + url + '">' + url + '</a></p>');
-};
+  var url = exports.loginPath;
+  if (!~url.indexOf('://')) {
+    url = 'http' + (req.connection.encrypted ? 's' : '') + '://' + req.headers.host + url;
+  }
+  res.writeHead(302, {"Location": url, "Content-Type": "text/html"});
+  res.end('<p>Redirecting to <a href="' + url + '">' + url + '</a></p>');
+}
+
 /**
  * All those roles will match any other role
  */
 exports.superAdmin = ["superadmin"];
+
 /**
  * req's key to populate
  */
 exports.reqKey = "user";
+
 /**
  * Load credentials from session
  */
 exports.retrieve = function (req, callback) {
-    var data = {};
-    if (req.session) {
-        data = req.session._credentials_data_ || data;
-        callback(data.loggedin || false, data.roles || []);
-    }
-    else {
-        callback(false, null);
-    }
-};
+  var data = {};
+  if (req.session) {
+    data = req.session._credentials_data_ || data;
+    callback(data.loggedin || false, data.roles || []);
+  } else {
+    callback(false, null);
+  }
+}
+
 /**
  * Save credentials to session
  */
 exports.save = function (req, loggedin, roles, callback) {
-    if (req.session) {
-        req.session._credentials_data_ = {
-            "loggedin": loggedin,
-            "roles": roles
-        };
-    }
-    callback();
-};
+  if (req.session) {
+    req.session._credentials_data_ = {
+      "loggedin": loggedin,
+      "roles":    roles
+    };
+  }
+  callback();
+}
+
 /**
  * Initialize roles when user logs in
  */
 exports.initialize = function (req, callback) {
-    callback(null);
-};
+  callback(null);
+}
+
 /**
  * role based access control middleware for connect
  *
  * Usage:
- *
+ * 
  * var ac = require('connect-access-control');
  * app.use(accessControl({
  *   ignoredPaths: [ ... routes to ignore, strings or regular expressions ... ] // default = "/favicon"
@@ -95,7 +108,7 @@ exports.initialize = function (req, callback) {
  *   retrieve: function (req, callback) { ... load credentials ... then call callback(loggedIn, roles = null if load failed) ... } // default = load from session
  *   save: function (req, loggedIn, roles, callback) { ... save credentials ... then call callback() ... } // default = save to session
  * }));
- *
+ * 
  * All configuration options are optional.
  *
  * To enable global protection, define accessControl.securedPaths or option securedPaths.
@@ -141,58 +154,59 @@ exports.initialize = function (req, callback) {
  *  - req.user.logout(callback)         // Logs user out and save
  *  - req.user.isSuperAdmin()           // Has a superadmin role
  */
-function accessControl(options) {
-    options = options || {};
-    var ignoredPaths = utils.merge(exports.ignoredPaths, options.ignoredPaths || []), securedPaths = utils.merge(exports.securedPaths, options.securedPaths || []), requiredLoggedIn = ('undefined' == typeof options.requiredLoggedIn) ? exports.requiredLoggedIn : options.requiredLoggedIn, requiredRoles = options.requiredRoles || exports.requiredRoles, superAdmin = utils.merge(exports.superAdmin, options.superAdmin || []), initialize = options.initialize || exports.initialize, retrieve = options.retrieve || exports.retrieve, save = options.save || exports.save;
-    return function (req, res, next) {
-        // ignored path ?
-        if (utils.match(req.pathname, ignoredPaths))
-            return next();
-        // Populate req.user
-        retrieve(req, function (loggedin, roles) {
-            if ('undefined' != typeof roles && roles !== null) {
-                try {
-                    req[exports.reqKey] = new User(loggedin, roles, {
-                        "req": req,
-                        "save": save,
-                        "superAdmin": superAdmin,
-                        "initialize": initialize
-                    });
-                }
-                catch (e) {
-                    return next(e);
-                }
-            }
-            // Protect path ?
-            if (req[reqKey] && utils.match(req.pathname, securedPaths)) {
-                exports.secure(requiredLoggedIn, function (req, res) {
-                    exports.secure(requiredRoles, next);
-                });
-            }
-            else {
-                next();
-            }
+function accessControl (options) {
+  options = options || {};
+  var ignoredPaths = utils.merge(exports.ignoredPaths, options.ignoredPaths || [])
+    , securedPaths = utils.merge(exports.securedPaths, options.securedPaths || [])
+    , requiredLoggedIn = ('undefined' == typeof options.requiredLoggedIn) ? exports.requiredLoggedIn : options.requiredLoggedIn
+    , requiredRoles = options.requiredRoles || exports.requiredRoles
+    , superAdmin = utils.merge(exports.superAdmin, options.superAdmin || [])
+    , initialize = options.initialize || exports.initialize
+    , retrieve = options.retrieve || exports.retrieve
+    , save = options.save || exports.save
+    ;
+  
+  return function (req, res, next) {
+    // ignored path ?
+    if (utils.match(req.pathname, ignoredPaths)) return next();
+    // Populate req.user
+    retrieve(req, function (loggedin, roles) {
+      if ('undefined' != typeof roles && roles !== null) {
+        try {
+          req[exports.reqKey] = new User(loggedin, roles, {
+            "req": req,
+            "save": save,
+            "superAdmin": superAdmin,
+            "initialize": initialize
+          });
+        } catch (e) {
+          return next(e);
+        }
+      }
+      // Protect path ?
+      if (req[reqKey] && utils.match(req.pathname, securedPaths)) {
+        exports.secure(requiredLoggedIn, function (req, res) {
+          exports.secure(requiredRoles, next);
         });
-    };
+      } else {
+        next();
+      }
+    });
+  }
 }
+
 /**
  * Require user to be logged in
  */
 exports.secure = function (roles) {
-    return function (req, res, next) {
-        if (!roles || (roles instanceof Array && roles.length == 0))
-            next();
-        else if (roles === true) {
-            if (req[reqKey].isLoggedIn())
-                next();
-            else
-                exports.notLoggedInCallback(req, res, next);
-        }
-        else {
-            if (req[reqKey].has(roles))
-                next();
-            else
-                exports.limitedAccessCallback(req, res, next);
-        }
-    };
-};
+  return function (req, res, next) {
+    if (!roles || (roles instanceof Array && roles.length == 0)) next();
+    else if (roles === true) {
+      if (req[reqKey].isLoggedIn()) next();
+      else exports.notLoggedInCallback(req, res, next);
+    } else {
+      if (req[reqKey].has(roles)) next();
+      else exports.limitedAccessCallback(req, res, next);
+    }
+  }
+}
