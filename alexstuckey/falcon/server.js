f8e1a8cdf69c16fbcb66cbceaa10793435222ee6
simon.gaozheng <simon.gaozheng@gmail.com>
2016-08-23 02:26:01 +0100

    annotation

--- alexstuckey/falcon/server.js
+++ alexstuckey/falcon/server.js
@@ -2,41 +2,35 @@ var amqp = require('amqp');
 var fs = require('fs');
 var onMessage = require('./onMessage.js');
 var amqp_hacks = require('./amqp-hacks.js');
-
 // Load the configuration JSON file
 var config = JSON.parse(fs.readFileSync('./config.json'));
-
 var connection = amqp.createConnection({
-  host: config.rabbit.host,
-  port: config.rabbit.port,
-  login: config.rabbit.username,
-  password: config.rabbit.password,
-  authMechanism: 'AMQPLAIN',
-  vhost: '/',
-  ssl:  { 
-          enabled : false
-        }
+    host: config.rabbit.host,
+    port: config.rabbit.port,
+    login: config.rabbit.username,
+    password: config.rabbit.password,
+    authMechanism: 'AMQPLAIN',
+    vhost: '/',
+    ssl: {
+        enabled: false
+    }
 });
-
 connection.on('ready', function onConnection() {
-  connection.queue('notifications', {
-      autoDelete: false,
-      durable: true
-    },
-    onQueue(queue));
+    connection.queue('notifications', {
+        autoDelete: false,
+        durable: true
+    }, onQueue(queue));
 });
-
 function onQueue(queue) {
-  console.log('Connected to RabbitMQ @', config.rabbit.host, "...");
-  console.log('To exit press CTRL-c');
-  queue.subscribe({ ack: true,
-                    prefetchCount: 1
-                  }, function(message, headers, deliveryInfo, queue){
-                    onMessage(message, headers, deliveryInfo, queue)
-                  });
+    console.log('Connected to RabbitMQ @', config.rabbit.host, "...");
+    console.log('To exit press CTRL-c');
+    queue.subscribe({ ack: true,
+        prefetchCount: 1
+    }, function (message, headers, deliveryInfo, queue) {
+        onMessage(message, headers, deliveryInfo, queue);
+    });
 }
-
 process.on('uncaughtException', function onUncaughtException(err) {
-  console.log(err.stack);
-  throw err;
+    console.log(err.stack);
+    throw err;
 });
